c620380e79fb8951cf972cb9a32702b011f73855
Jason Sourivong <serjason2@gmail.com>
2025-04-12 23:03:45 -0500

    Responds with requested CVE

--- CYB101/CIRCL_CVE_Lookup.py
+++ CYB101/CIRCL_CVE_Lookup.py
@@ -3,3 +3,21 @@
 # Looking up a given CVE using CIRCL
 # Week 8 Project 7: Threat Analysis
 
+import requests
+import json
+
+def get_cve(cve):
+    try:
+        response = requests.get(f"https://cve.circl.lu/api/cve/{cve}")
+        response.raise_for_status()
+        result = response.json()
+        print(json.dumps(result, indent=4))
+
+        return result
+    
+    except:
+        return 0
+    
+    return None
+
+get_cve("CVE-2015-3194")
